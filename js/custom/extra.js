c967b68504965725b4de3bdc1ee0d219e03bf9de
muhammed atlı <mega.sus20@gmail.com>
2023-04-17 23:33:08 +0300

    footer end

--- js/custom/extra.js
+++ js/custom/extra.js
@@ -27,3 +27,10 @@ for (let i = 0; i < BtnCanvas.length; i++) {
         document.querySelector('[data-bs-dismiss="offcanvas"]').click();
     });
 }
+
+var BtnCanvas = document.querySelectorAll(".btn-close-canvas");
+for (let i = 0; i < BtnCanvas.length; i++) {
+    BtnCanvas[i].addEventListener("click", function () {
+        document.querySelector('[data-bs-dismiss="offcanvas"]').click();
+    });
+}
